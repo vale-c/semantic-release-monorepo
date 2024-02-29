e273f4e622c1c7a9735a3f35e6136a5715f9fa39
Valentina Calabrese <valentina.calabrese@remote.com>
2024-02-29 21:54:08 +0100

    latest changes

---
+++ apps/app_one/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ExampleComponent } from 'design-system';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ExampleComponent text="Hello from app_one" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
